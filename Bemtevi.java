2e12bb1f485335ba02785dc4e2277d0582aff999
Cauê (vvankxs7) <cauecpereira06@gmail.com>
2026-03-22 01:38:31 -0300

    Add files via upload

---
+++ Bemtevi.java
@@ -0,0 +1,17 @@
+public class Bemtevi{
+    private String bloco;
+    private float num;
+    
+    public Bemtevi(String bloco,float num){;
+        this.bloco = bloco;
+        this.num = num;
+}
+
+    public Bemtevi(String bloco) {
+        this("assara", 0.0f);
+    }
+    
+    public Bemtevi(float num){
+        this("",0.0f);
+    }
+}
