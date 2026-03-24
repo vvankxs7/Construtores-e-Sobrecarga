0db86eca06da84ea987d35ac928974dff5dc8a47
Cauê (vvankxs7) <cauecpereira06@gmail.com>
2026-03-24 14:40:47 -0300

    Add DemoAluno2 class with main method

---
+++ DemoAluno2.java
@@ -0,0 +1,11 @@
+public class DemoAluno2{
+    public static void main(String[] args){
+        Aluno2 a = new Aluno2("Cauê",8.5f);
+        Aluno2 b = new Aluno2("Agebson");
+        Aluno2 c = new Aluno2();
+        
+        System.out.println("Aluno a: "+a);
+        System.out.println("Aluno b: "+b);
+        System.out.println("Aluno c: "+c);
+    }
+}
